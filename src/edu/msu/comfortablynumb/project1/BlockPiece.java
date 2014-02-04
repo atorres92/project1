14dae2e250afab69680be7d3fee2605b06672575
russelan <russelan.msu>
2014-02-04 16:17:22 -0500

    Added Center of mass calculation

--- src/edu/msu/comfortablynumb/project1/BlockPiece.java
+++ src/edu/msu/comfortablynumb/project1/BlockPiece.java
@@ -7,11 +7,11 @@ import android.graphics.Canvas;
 
 public class BlockPiece {
 
-	
+
 	private int id;
-	
-	
-	
+
+
+
 	public int getId() {
 		return id;
 	}
@@ -24,24 +24,64 @@ public class BlockPiece {
 
 	//the image of the block
 	private Bitmap block;
-	
+
 	/**
-	 * x location 
+	 * x location
 	 */
 	private float x;
 	/**
 	 * y location
 	 */
 	private float y;
+	/**
+	 * weight
+	 */
+	private int weight;
 
-	
+
+	public float getX() {
+		return x;
+	}
+
+
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+
+
+	public float getY() {
+		return y;
+	}
+
+
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+
+
+	public int getWeight() {
+		return weight;
+	}
+
+
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+
+
 	public BlockPiece(Context context, int id) {
 		this.id = id;
-		
+
 		block = BitmapFactory.decodeResource(context.getResources(), id);
-		
+
 	}
-	
+
 	public void draw(Canvas canvas ){
 		canvas.drawBitmap(block, 0, 0,null);
 	}
--- src/edu/msu/comfortablynumb/project1/Game.java
+++ src/edu/msu/comfortablynumb/project1/Game.java
@@ -11,26 +11,26 @@ import android.view.View;
 public class Game {
 
 	public ArrayList<BlockPiece> blocks = new ArrayList<BlockPiece>();
-	
+
 	private BlockView blockView;
-	
+
 	public Game(Context context, View view) {
 		blockView = (BlockView) view;
-		
+
 		blocks.add(new BlockPiece(context, R.drawable.brick_barney));
 		blocks.add(new BlockPiece(context, R.drawable.brick_green1));
 	}
-	
+
 	public void draw(Canvas canvas){
 		for(BlockPiece blockPiece : blocks){
 			blockPiece.draw(canvas);
 		}
-		
+
 	}
-	
-	
+
+
 	public boolean onTouchEvent(View view, MotionEvent event){
-		
+
 		switch(event.getActionMasked()){
 		case MotionEvent.ACTION_DOWN:
 			//onTouched(event.getX(), event.getY());
@@ -41,13 +41,38 @@ public class Game {
 			break;
 		case MotionEvent.ACTION_UP:
 			break;
-			
+
 		}
 		return false;
 	}
-	
+
 	private boolean onTouched(float x, float y){
 		return true;
+	}
+
+	/*returns float array of center of mass:
+	 * value[0] = X
+	 * value[1] = Y
+	 */
+	private float[] calculateCenterOfMass(){
+
+		//Sum up the blocks to find center of mass
+		float[] centerOfMass = new float[2];
+		int sumMass = 0;
+		float sumX = 0;
+		float sumY = 0;
+
+		//Remember to remove the first one from calculation later
+		for (BlockPiece b : blocks){
+	        sumMass = sumMass + b.getWeight();
+	        sumX = ((float)b.getWeight()) * b.getX();
+	        sumY = ((float)b.getWeight()) * b.getY();
+
+	     }
+		centerOfMass[0] = (float) (1.0 / ((float)sumMass) *sumX);
+		centerOfMass[1] = (float) (1.0 / ((float)sumMass) *sumY);
+
+		return centerOfMass;
 	}
 
 }
