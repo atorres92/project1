e79508826b588588dd7d29af0f508f74b9a5c14d
Aaron Torres <torresaa92@gmail.com>
2014-02-05 15:58:26 -0500

    Created Startup screen with Player 1 and 2 input name boxes

--- src/edu/msu/comfortablynumb/project1/MainActivity.java
+++ src/edu/msu/comfortablynumb/project1/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	final String PLAYER_ONE_NAME = "PLAYER_ONE_NAME";
+	final String PLAYER_TWO_NAME = "PLAYER_TWO_NAME";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,7 +18,17 @@ public class MainActivity extends Activity {
 	}
 	
 	public void onStartGame(View view) {
+		EditText playerOneText = (EditText) findViewById(R.id.player1Input);
+		EditText playerTwoText = (EditText) findViewById(R.id.player2Input);
+		
+		String playerOneName = playerOneText.getText().toString();
+		String playerTwoName = playerTwoText.getText().toString();
+		
 		Intent intent = new Intent(this, GameActivity.class);
+		
+		intent.putExtra(PLAYER_ONE_NAME, playerOneName);
+		intent.putExtra(PLAYER_TWO_NAME, playerTwoName);
+
 		startActivity(intent);
 	}
 
