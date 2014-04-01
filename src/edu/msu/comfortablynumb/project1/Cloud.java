0b7e83447182a8c2b9ef2afdf953e343cbcb9ade
russelan <russelan.msu>
2014-04-01 14:01:19 -0400

    Added account creation on server

---
+++ src/edu/msu/comfortablynumb/project1/Cloud.java
@@ -0,0 +1,208 @@
+package edu.msu.comfortablynumb.project1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlSerializer;
+
+import android.util.Log;
+import android.util.Xml;
+
+public class Cloud {
+
+	private static final String MAGIC = "NechAtHa6RuzeR8x";
+
+	//Fix this URL
+	private static final String CREATE_URL = "https://www.cse.msu.edu/~patelke6/cse476/brick-create.php";
+	private static final String LOGON_URL = "https://www.cse.msu.edu/~patelke6/cse476/brick-login.php";
+
+	private static final String UTF8 = "UTF-8";
+
+	   /**
+     * Save a hatting to the cloud.
+     * This should be run in a thread.
+     * @param name name to save under
+     * @param view view we are getting the data from
+     * @return true if successful
+     */
+    public boolean saveOnCloud(String username, String password, boolean newuser) {
+        username = username.trim();
+        if(username.length() == 0) {
+            return false;
+        }
+
+        // Create an XML packet with the information about the current image
+        XmlSerializer xml = Xml.newSerializer();
+        StringWriter writer = new StringWriter();
+
+        try {
+            xml.setOutput(writer);
+
+            xml.startDocument("UTF-8", true);
+
+            xml.startTag(null, "stacker");
+
+            xml.attribute(null, "user", username);
+            xml.attribute(null, "pw", password);
+            xml.attribute(null, "magic", MAGIC);
+            if(newuser)
+            	xml.attribute(null, "newuser", "1");
+            else
+            	xml.attribute(null, "newuser", "0");
+
+            xml.endTag(null, "stacker");
+
+            xml.endDocument();
+
+        } catch (IOException e) {
+            // This won't occur when writing to a string
+            return false;
+        }
+
+        final String xmlStr = writer.toString();
+
+        /*
+         * Convert the XML into HTTP POST data
+         */
+        String postDataStr;
+        try {
+            postDataStr = "xml=" + URLEncoder.encode(xmlStr, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return false;
+        }
+
+        /*
+         * Send the data to the server
+         */
+        byte[] postData = postDataStr.getBytes();
+
+        InputStream stream = null;
+        try {
+            URL url = new URL(CREATE_URL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Length", Integer.toString(postData.length));
+            conn.setUseCaches(false);
+
+            OutputStream out = conn.getOutputStream();
+            out.write(postData);
+            out.close();
+
+            int responseCode = conn.getResponseCode();
+            if(responseCode != HttpURLConnection.HTTP_OK) {
+                return false;
+            }
+
+            stream = conn.getInputStream();
+            //logStream(stream);
+
+            /**
+             * Create an XML parser for the result
+             */
+            try {
+                XmlPullParser xmlR = Xml.newPullParser();
+                xmlR.setInput(stream, UTF8);
+
+                xmlR.nextTag();      // Advance to first tag
+                xmlR.require(XmlPullParser.START_TAG, null, "hatter");
+
+                String status = xmlR.getAttributeValue(null, "status");
+                if(status.equals("no")) {
+                    return false;
+                }
+
+                // We are done
+            } catch(XmlPullParserException ex) {
+                return false;
+            } catch(IOException ex) {
+                return false;
+            }
+
+        } catch (MalformedURLException e) {
+            return false;
+        } catch (IOException ex) {
+            return false;
+        } finally {
+            if(stream != null) {
+                try {
+                    stream.close();
+                } catch(IOException ex) {
+                    // Fail silently
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Save a hatting to the cloud.
+     * This should be run in a thread.
+     * @param name name to save under
+     * @param view view we are getting the data from
+     * @return true if successful
+     */
+    public InputStream createOnCloud(String username, String password, boolean newuser) {
+        username = username.trim();
+        if(username.length() == 0) {
+            return null;
+        }
+
+
+        // Create a get query
+        String query = CREATE_URL + "?user=" + username + "&magic=" + MAGIC + "&pw=" + password;
+
+        try {
+            URL url = new URL(query);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            int responseCode = conn.getResponseCode();
+            if(responseCode != HttpURLConnection.HTTP_OK) {
+                return null;
+            }
+
+            InputStream stream = conn.getInputStream();
+            logStream(stream);
+            return stream;
+
+        } catch (MalformedURLException e) {
+            // Should never happen
+            return null;
+        } catch (IOException ex) {
+
+            Log.i("Execpetion", ex.getMessage());
+            return null;
+        }
+    }
+
+    public static void logStream(InputStream stream) {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(stream));
+
+        Log.e("476", "logStream: If you leave this in, code after will not work!");
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Log.e("476", line);
+            }
+        } catch (IOException ex) {
+            return;
+        }
+    }
+
+
+}
--- src/edu/msu/comfortablynumb/project1/CreateAccountActivity.java
+++ src/edu/msu/comfortablynumb/project1/CreateAccountActivity.java
@@ -1,8 +1,18 @@
 package edu.msu.comfortablynumb.project1;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -48,13 +58,49 @@ public class CreateAccountActivity extends Activity {
         else if (passwordText.compareTo(passwordConfirmText) != 0) {
             Toast.makeText(this, PASSWORDS_DONT_MATCH, Toast.LENGTH_SHORT).show();
         } else {
-            //
-            //
-            //
-            //
-            //Create account server code here
-            //
-            //
+
+            // Create a thread to create a new login
+            new Thread(new Runnable() {
+
+                /**
+                 * Save the hatting in the background
+                 */
+                @Override
+                public void run() {
+                    Cloud cloud = new Cloud();
+                    InputStream stream = cloud.createOnCloud(username.getText().toString(), password.getText().toString(), true);
+
+                    // Test for an error
+                    boolean fail = stream == null;
+                    if(!fail) {
+                        try {
+                            XmlPullParser xml = Xml.newPullParser();
+                            xml.setInput(stream, "UTF-8");
+
+
+                            xml.nextTag();      // Advance to first tag
+                            xml.require(XmlPullParser.START_TAG, null, "brick");
+                            String status = xml.getAttributeValue(null, "status");
+                            Log.i("Stream","status");
+
+
+                        } catch(IOException ex) {
+                            fail = true;
+                        } catch(XmlPullParserException ex) {
+                            fail = true;
+                        } finally {
+                            try {
+                                stream.close();
+                            } catch(IOException ex) {
+                            }
+                        }
+                    }
+                }
+
+
+
+            }).start();
+
             finish(); //Return back to login screen
         }
     }
